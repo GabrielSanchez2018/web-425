caef3a5ab84b37bbcd6ea9279e0bf41f4b61be60
Sanchez Serna <gjsanchez2@mail.mccneb.edu>
2020-01-12 20:50:02 -0600

    week-2

---
+++ week-2/sanchez-one-way-binding/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>{{ name }}</h1>
+    <button (click)="changeName()">Change Name</button>
+  `,
+  styles: [
+    `
+    h1 {
+      color: red;
+    }
+    `
+  ]
+
+})
+export class AppComponent {
+  title = 'sanchez-one-way-binding';
+
+  // class property 
+  name: string = "Professor krasso";
+
+  //Change name Function 
+  changeName() {
+    this.name = "Richard Krasso";
+  }
+}
+
+// In order to run the program ng serve -o
