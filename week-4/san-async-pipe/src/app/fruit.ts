22cb996bdecdf6f652403f3bc3a37a8f6161fca2
Sanchez Serna <gjsanchez2@mail.mccneb.edu>
2020-01-27 00:35:08 -0600

    week-4

--- week-4/san-async-pipe/src/app/fruit.ts
+++ week-4/san-async-pipe/src/app/fruit.ts
@@ -1,4 +1,12 @@
-
+/*
+============================================
+; Title: Assignment 4.4
+; Author: Professor Krasso
+; Date: 1/26/2020
+; Modified By: Gabriel Sanchez
+; Description: asynchronous pipes and observable arrays
+;===========================================
+*/
 export interface Fruit {
     id: number;
     name: string;
